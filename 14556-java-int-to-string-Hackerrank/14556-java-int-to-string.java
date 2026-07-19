e2ad60eb2205e86c0a90e397af5c8f2c3af0b187
RUPAM MONDAL <mondalrupam756@gmail.com>
2026-07-19 16:06:35 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 14556-java-int-to-string-Hackerrank/14556-java-int-to-string.java
@@ -0,0 +1,24 @@
+//The following class will prevent you from terminating the code using 
+ }
+}
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+    System.out.println("Wrong answer.");
+   }
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+
+   
+exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
